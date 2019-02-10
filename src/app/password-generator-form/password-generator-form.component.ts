a05ca69851332564cda0ad93ae297cf048eba5f1
namanattri <namanattri5@gmail.com>
2019-02-10 15:01:50 +0530

    Optimize password generator service.

--- src/app/password-generator-form/password-generator-form.component.ts
+++ src/app/password-generator-form/password-generator-form.component.ts
@@ -11,29 +11,28 @@ import { CopyToClipboardService } from '../copy-to-clipboard.service';
 export class PasswordGeneratorFormComponent implements OnInit {
 
   passwordLengthMin: number = 16
-  passwordLengthMax: number = 100
+  passwordLengthMax: number = 200
 
   passwordGeneratorForm: FormGroup = new FormGroup({
-    useUppercaseLetters: new FormControl(true),
-    useLowercaseLetters: new FormControl(true),
-    useNumbers: new FormControl(true),
-    useSymbols: new FormControl(true),
-    useArithmeticOperators: new FormControl(true),
-    useSpecialCharacters: new FormControl(true),
-    allowSpaces: new FormControl(false),
-    allowRepeatingCharacters: new FormControl(false),
+    useUppercaseLetters: new FormControl(),
+    useLowercaseLetters: new FormControl(),
+    useNumbers: new FormControl(),
+    useSymbols: new FormControl(),
+    useArithmeticOperators: new FormControl(),
+    useSpecialCharacters: new FormControl(),
+    allowSpaces: new FormControl(),
+    allowRepeatingCharacters: new FormControl(),
 
-    uppercaseLetters: new FormControl('ABCDEFGHIJKLMNOPQRSTUVWXYZ'),
-    lowercaseLetters: new FormControl('abcdefghijklmnopqrstuvwxyz'),
-    numbers: new FormControl('0123456789'),
-    symbols: new FormControl('!@#$&?'),
-    arithmeticOperators: new FormControl('%*+-=/'),
-    specialCharacters: new FormControl('~^()_`{}|[]\:";<>?,.\'"'),
+    uppercaseLetters: new FormControl(),
+    lowercaseLetters: new FormControl(),
+    numbers: new FormControl(),
+    symbols: new FormControl(),
+    arithmeticOperators: new FormControl(),
+    specialCharacters: new FormControl(),
 
-    passwordLength: new FormControl(16),
+    passwordLength: new FormControl(),
 
-
-    generatedPassword: new FormControl('')
+    generatedPassword: new FormControl()
   })
 
   generatedPassword: string = "";
@@ -43,7 +42,52 @@ export class PasswordGeneratorFormComponent implements OnInit {
     private copyToClipboardService: CopyToClipboardService
   ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.initForm()
+
+    this.passwordGeneratorForm.valueChanges.subscribe(formState => {
+      if(!formState.allowRepeatingCharacters) {
+        this.passwordLengthMax = formState.uppercaseLetters.length +
+          formState.lowercaseLetters.length +
+          formState.numbers.length +
+          formState.symbols.length +
+          formState.arithmeticOperators.length +
+          formState.specialCharacters.length
+      } else {
+        this.passwordLengthMax = 200
+      }
+
+      // if current selected length is > max set it to max
+      if(formState.passwordLength > this.passwordLengthMax)
+      this.passwordGeneratorForm.patchValue({
+        passwordLength: this.passwordLengthMax
+      })
+    })
+  }
+
+  initForm() {    
+    this.passwordGeneratorForm.patchValue({
+      useUppercaseLetters: true,
+      useLowercaseLetters: true,
+      useNumbers: true,
+      useSymbols: true,
+      useArithmeticOperators: true,
+      useSpecialCharacters: true,
+      allowSpaces: false,
+      allowRepeatingCharacters: false,
+
+      uppercaseLetters: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+      lowercaseLetters: 'abcdefghijklmnopqrstuvwxyz',
+      numbers: '0123456789',
+      symbols: '!@#$&?',
+      arithmeticOperators: '%*+-=/',
+      specialCharacters: '~^()_`{}|[]\:";<>,.\'',
+
+      passwordLength: 16,
+  
+      generatedPassword: ''
+    })
+  }
 
   generate() {
     console.log(this.passwordGeneratorForm.value);
@@ -63,6 +107,10 @@ export class PasswordGeneratorFormComponent implements OnInit {
     this.passwordGeneratorForm.patchValue({
       passwordLength: value
     })
+  }
+
+  resetForm() {
+    this.initForm()
   }
 
 }
--- src/app/password-generator.service.ts
+++ src/app/password-generator.service.ts
@@ -7,52 +7,95 @@ import { PasswordGeneratorFormInterface } from './password-generator-form-interf
 export class PasswordGeneratorService {
 
   allowRepeatingCharacters: boolean;
+  unshuffledPassword: string = "";
 
-  generate(formValue: PasswordGeneratorFormInterface) {    
-    let unshuffledPassword: string = "";
+  uppercaseLetters: string = "";
+  lowercaseLetters: string = "";
+  numbers: string = "";
+  symbols: string = "";
+  arithmeticOperators: string = "";
+  specialCharacters: string = "";
+  
+
+  generate(formValue: PasswordGeneratorFormInterface): string {
+    this.unshuffledPassword = "";
+
     this.allowRepeatingCharacters = formValue.allowRepeatingCharacters;
 
-    if(formValue.useUppercaseLetters) unshuffledPassword += this.getRandom(formValue.uppercaseLetters, 1);
-    if(formValue.useLowercaseLetters) unshuffledPassword += this.getRandom(formValue.lowercaseLetters, 1);
-    if(formValue.useNumbers) unshuffledPassword += this.getRandom(formValue.numbers, 1);
-    if(formValue.useSymbols) unshuffledPassword += this.getRandom(formValue.symbols, 1);
-    if(formValue.useArithmeticOperators) unshuffledPassword += this.getRandom(formValue.arithmeticOperators, 1);
-    if(formValue.useSpecialCharacters) unshuffledPassword += this.getRandom(formValue.specialCharacters, 1);
-    if(formValue.allowSpaces) unshuffledPassword += this.getRandom(" ", 1);
+    this.uppercaseLetters = formValue.uppercaseLetters;
+    this.lowercaseLetters = formValue.lowercaseLetters;
+    this.numbers = formValue.numbers;
+    this.symbols = formValue.symbols;
+    this.arithmeticOperators = formValue.arithmeticOperators;
+    this.specialCharacters = formValue.specialCharacters;
 
-    if(unshuffledPassword.length < formValue.passwordLength) {
-      unshuffledPassword += this.getRandom(
-        (formValue.useUppercaseLetters ? formValue.uppercaseLetters : "")
-          + (formValue.useLowercaseLetters ? formValue.lowercaseLetters : "")
-          + (formValue.useNumbers ? formValue.numbers : "")
-          + (formValue.useSymbols ? formValue.symbols : "")
-          + (formValue.useArithmeticOperators ? formValue.arithmeticOperators : "")
-          + (formValue.useSpecialCharacters ? formValue.specialCharacters : "")
-          + (formValue.allowSpaces ? " " : ""),
-          formValue.passwordLength - unshuffledPassword.length);
+    //prefix
+    if(formValue.useUppercaseLetters) this.unshuffledPassword += this.getRandom(formValue.uppercaseLetters, 1);
+    if(formValue.useLowercaseLetters) this.unshuffledPassword += this.getRandom(formValue.lowercaseLetters, 1);
+    if(formValue.useNumbers) this.unshuffledPassword += this.getRandom(formValue.numbers, 1);
+    if(formValue.useSymbols) this.unshuffledPassword += this.getRandom(formValue.symbols, 1);
+    if(formValue.useArithmeticOperators) this.unshuffledPassword += this.getRandom(formValue.arithmeticOperators, 1);
+    if(formValue.useSpecialCharacters) this.unshuffledPassword += this.getRandom(formValue.specialCharacters, 1);
+    if(formValue.allowSpaces) this.unshuffledPassword += this.getRandom(" ", 1);
+
+    //suffix
+    if(this.unshuffledPassword.length < formValue.passwordLength) {
+      let combo = (formValue.useUppercaseLetters ? formValue.uppercaseLetters : "")
+      + (formValue.useLowercaseLetters ? formValue.lowercaseLetters : "")
+      + (formValue.useNumbers ? formValue.numbers : "")
+      + (formValue.useSymbols ? formValue.symbols : "")
+      + (formValue.useArithmeticOperators ? formValue.arithmeticOperators : "")
+      + (formValue.useSpecialCharacters ? formValue.specialCharacters : "")
+      + (formValue.allowSpaces ? " " : "");
+
+      if(!this.allowRepeatingCharacters) {
+        combo = this.removeUsed(combo);
+        this.unshuffledPassword += this.getRandomUnique(combo, formValue.passwordLength - this.unshuffledPassword.length)        
+      } else {
+        // repeatitive works
+        this.unshuffledPassword += this.getRandom(combo, formValue.passwordLength - this.unshuffledPassword.length)        
+      }
     }
 
-    return this.shuffle(unshuffledPassword);
+    return this.shuffle(this.unshuffledPassword);
   }
 
-  private shuffle(str: string) {
+  private removeUsed(combo: string): string {
+    this.unshuffledPassword.split('').forEach(char => {
+      combo = combo.replace(char, '')
+    })
+    return combo;
+  }
+
+  private shuffle(str: string): string {
     if (!str) return ''
     return str.split('').sort((a, b) => Math.random()>.5 ? -1 : 1).join('');
   }
 
-  private getRandom(str: string, len: number) {
+  private getRandom(str: string, len: number): string {
     let randomTxt: string = "";
-    console.log("string: ", str)
+    
+    if(str.length == 0) return randomTxt;
+    
     for(let i:number = 0; i < len; i++) {
       randomTxt += this.getRandomChar(str, randomTxt)
     }
     return randomTxt;
   }
 
-  private getRandomChar(str: string, randomTxt: string){
-    let char = str.charAt(Math.floor(Math.random() * str.length))
+  private getRandomUnique(str: string, len: number): string {
+    let randomTxt: string = "";
+    
+    for(let i:number = 0; i < len; i++) {
+      let ch = this.getRandomChar(str, randomTxt)
+      randomTxt += ch;
+      str = str.replace(ch, '')
+    }
+    return randomTxt;
+  }
 
-    if(!this.allowRepeatingCharacters && randomTxt.includes(char)) return this.getRandomChar(str, randomTxt)
-    else return char;
+  private getRandomChar(str: string, randomTxt: string): string {
+    let char = str.charAt(Math.floor(Math.random() * str.length))
+    return char;
   }
 }
